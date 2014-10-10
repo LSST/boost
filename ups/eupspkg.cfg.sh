f9f4b1e073c9017e6a0665324dc27d92b9c0f320
Russell Owen <rowen@uw.edu>
2014-10-10 14:47:26 -0700

    fixup! fixup! Build with C++11

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -21,20 +21,20 @@ build()
 	set +e
 	c++ -o ups/test_cpp11.o -std=c++11 ups/trivial.cc 2>/dev/null
 	if (( $? == 0 )); then
-		cxxflags="-std=c++11"
+		cxxstring_str="-std=c++11"
 	else
-		cxxflags="-std=c++0x"
+		cxxstring_str="-std=c++0x"
 	fi
-	c++ -o ups/test_warn_deprecated_register.o $cxxflags -Wno-deprecated-register ups/trivial.cc 2>/dev/null
+	c++ -o ups/test_warn_deprecated_register.o $cxxstring_str -Wno-deprecated-register ups/trivial.cc 2>/dev/null
 	if (( $? == 0 )); then
-		cxxflags="$cxxflags -Wno-deprecated-register"
+		cxxstring_str="$cxxstring_str -Wno-deprecated-register"
 	fi
 	set -e
-	# double quotes are required if there are spaces in cxxflags
-	cxxflags = "\"$cxxflags"\"
-	echo "Building boost with cxxflags=$cxxflags"
+	# double quotes are required if there are spaces in cxxstring_str
+	cxxstring_str = "\"$cxxstring_str"\"
+	echo "Building boost with cxxflags=$cxxstring_str"
 
-	./b2 -j $NJOBS cxxflags=$cxxflags
+	./b2 -j $NJOBS cxxflags=$cxxstring_str
 }
 
 install()
